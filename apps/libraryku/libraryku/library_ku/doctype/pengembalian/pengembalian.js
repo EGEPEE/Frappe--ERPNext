a5a97c6b77fcd847144ded41cb3100e2d2188e9d
Ega Prasetianti <eprasetianti@gmail.com>
2019-04-03 10:54:41 +0700

    first commit

---
+++ apps/libraryku/libraryku/library_ku/doctype/pengembalian/pengembalian.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Ega Prasetianti and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pengembalian', {
+	refresh: function(frm) {
+
+	}
+});
